3231b8b9209307ad33e5edcc7e9e66b566347669
Guylian Cox <hello@guylian.me>
2020-07-24 12:17:39 +0200

    feat: v3.0.0 - enabled new rules

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
-{
+module.exports = {
   "rules": {
     "color-hex-case": "lower",
     "color-hex-length": "long",
+    "color-no-invalid-hex": true,
+    "alpha-value-notation": "number",
+    "color-function-notation": "modern",
+
+    // TODO replace with denylist once available
+    "function-blacklist": ["rgba", "hsla"],
+
     "font-family-name-quotes": "always-where-recommended",
     "font-family-no-duplicate-names": true,
     "function-calc-no-unspaced-operator": true,
+    "function-calc-no-invalid": true,
     "function-comma-newline-after": "always-multi-line",
     "function-comma-space-after": "always-single-line",
     "function-comma-space-before": "never",
@@ -13,17 +21,27 @@
     "function-name-case": "lower",
     "function-parentheses-space-inside": "never-single-line",
     "function-url-quotes": "always",
+    "function-url-no-scheme-relative": true,
     "function-whitespace-after": "always",
     "number-leading-zero": "always",
     "number-no-trailing-zeros": true,
     "string-no-newline": true,
     "string-quotes": "single",
-    "length-zero-no-unit": true,
+
+    // fails with
+    // calc(env(safe-area-inset-left, 0px) + 16px);
+    "length-zero-no-unit": null,
+    // "length-zero-no-unit": [true, {
+    //   "ignore": ["custom-properties"]
+    // }],
     "unit-case": "lower",
     "unit-no-unknown": true,
+
+    // fails with "composes: flatList from './flat-list.scss';"
+    // "value-keyword-case": "lower",
     "value-keyword-case": null,
     "value-no-vendor-prefix": true,
-    "value-list-comma-space-after": "always",
+    "value-list-comma-space-after": "always-single-line",
     "value-list-comma-space-before": "never",
     "value-list-max-empty-lines": 1,
     "shorthand-property-no-redundant-values": true,
@@ -64,15 +82,20 @@
     "selector-combinator-space-before": "always",
     "selector-descendant-combinator-no-non-space": true,
     "selector-no-vendor-prefix": true,
-    "selector-pseudo-class-case": "lower",
 
+    "selector-pseudo-class-case": "lower",
     "selector-pseudo-class-parentheses-space-inside": "never",
+    "selector-pseudo-class-no-unknown": [true, {
+      "ignorePseudoClasses": ["global"]
+    }],
 
     "selector-pseudo-element-case": "lower",
     "selector-pseudo-element-colon-notation": "double",
     "selector-pseudo-element-no-unknown": true,
     "selector-type-case": "lower",
-    "selector-type-no-unknown": true,
+    "selector-type-no-unknown": [true, {
+      "ignore": ["custom-elements"]
+    }],
     "selector-max-empty-lines": 0,
     "selector-list-comma-newline-after": "always",
     "selector-list-comma-newline-before": "never-multi-line",
@@ -98,7 +121,10 @@
     "at-rule-name-space-after": "always",
     "at-rule-no-vendor-prefix": true,
     "at-rule-semicolon-newline-after": "always",
-
+    "at-rule-no-unknown": [true, {
+      // SCSS at rules
+      "ignoreAtRules": ["include", "value", "mixin", "function", "if", "return"]
+    }],
     "comment-empty-line-before": ["always", {
       "ignore": ["after-comment"]
     }],
@@ -106,21 +132,25 @@
     "comment-whitespace-inside": "always",
 
     "indentation": 2,
+    "linebreaks": "unix",
     "max-empty-lines": 1,
     "max-line-length": [170, {
       "ignore": ["comments"]
     }],
+    "no-eol-whitespace": true,
     "no-extra-semicolons": true,
     "no-invalid-double-slash-comments": true,
     "no-missing-end-of-source-newline": true,
 
-    "selector-pseudo-class-no-unknown": null,
-    "at-rule-no-unknown": null,
     "no-descending-specificity": null,
 
     "rule-empty-line-before": ["always", {
       "ignore": ["after-comment"],
       "except": ["first-nested"]
-    }]
+    }],
+
+    "declaration-no-important": true,
+    "keyframe-declaration-no-important": true,
+    "no-duplicate-at-import-rules": true
   }
 }
